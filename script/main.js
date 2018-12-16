b32009ff1fdbe2b63fff62993871979c78c59310
MikeBY2018 <babskij2015@gmail.com>
2018-12-16 18:26:14 +0300

    main processing 3

--- script/main.js
+++ script/main.js
@@ -1,6 +1,15 @@
+var clickTab = document.querySelectorAll('.tablinks');
 
-    
-    
+clickTab[0].onclick = function(){
+    openCity(event, 'standart');
+}
+clickTab[1].onclick = function(){
+    openCity(event, 'business');
+}
+
+// Get the element with id="defaultOpen" and click on it
+clickTab[1].click();
+
 function openCity(evt, mark) {
     // Declare all variables
     var i, tabcontent, tablinks;
@@ -18,8 +27,64 @@ function openCity(evt, mark) {
     }
   
     // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(mark).style.display = "block";
+    document.getElementById(mark).style.display = "flex";
     evt.currentTarget.className += " active";
 }
 
+// slides
+var prevVolvo, nextVolvo, prevVaz, nextVaz;
+prevVolvo = document.querySelector('.slider_volvo .prev');
+nextVolvo = document.querySelector('.slider_volvo .next');
+prevVaz = document.querySelector('.slider_vaz .prev');
+nextVaz = document.querySelector('.slider_vaz .next');
 
+
+prevVolvo.addEventListener('click',function(){
+    plusSlides(-1);
+});
+nextVolvo.addEventListener('click',function(){
+    plusSlides(1);
+});
+prevVaz.addEventListener('click',function(){
+    plusSlidesvz(-1);
+});
+nextVaz.addEventListener('click',function(){
+    plusSlidesvz(1);
+});
+
+var slideIndex = 1;
+showSlides(slideIndex);
+showSlidesvz(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function plusSlidesvz(n) {
+  showSlidesvz(slideIndex += n);
+}
+
+
+function showSlides(n) {
+  var i;
+  var slides = document.querySelectorAll(".slider_volvo .mySlides");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  slides[slideIndex-1].style.display = "block";
+  console.log(slides); 
+}
+
+function showSlidesvz(n) {
+  var i;
+  var slides = document.querySelectorAll(".slider_vaz .mySlides");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  slides[slideIndex-1].style.display = "block";
+  console.log(slides); 
+}
